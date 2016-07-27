9349a4c82a68c1cda5bd2fb3d38545c2bb0d85e8
Ryan Woo <rjwoo@sfu.ca>
2016-07-27 11:00:25 -0700

    added VCR test to organization model

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
+<<<<<<< 855850fb33300992fce36376a9e198c137acf71f
   describe "validations" do
     it "should have reasonable tech team size" do
       o = Organization.new FactoryGirl.attributes_for(:organization).merge({tech_team_size: 1000})
@@ -27,5 +28,10 @@ RSpec.describe Organization, type: :model do
       o = Organization.new FactoryGirl.attributes_for(:organization).merge({employee_count: 0})
       expect(o).to be_invalid
     end
+
+  it "creates a single pin of V6B 1G8", :vcr do
+    o = Organization.create(address: "142 W Hastings St, Vancouver, BC V6B 1G8, Canada")
+    result = o.valid?
+    expect(o.latitude.round(2)).to eq(49.28)
   end
 end
