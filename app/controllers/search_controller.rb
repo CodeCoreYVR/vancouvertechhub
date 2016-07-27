5f874727001070ec201813ac3451c60032d6c07d
david <David.Leehr@gmail.com>
2016-07-27 11:48:03 -0700

    implement combined search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,30 +2,39 @@ class SearchController < ApplicationController
 
    # The organization_display.jsx react component sends an ajax request with the appropriate parameters
   def search
-    if params[:term]
-      @results.concat(Organization.where('name ILIKE ? OR overview ILIKE ?', "%#{params[:term]}%", "%#{params[:term]}%"))
-    end
-    if params[:size]
-      if params[:size] == "1"
-        @results.concat(Organization.where(tech_team_size: 1..25))
-      elsif params[:size] == "2"
-        @results.concat(Organization.where(tech_team_size: 26..50))
-      elsif params[:size] == "3"
-        @results.concat(Organization.where('tech_team_size > ?', 50))
-      end
-    end
-    if params[:tech] != ""
-      tech = []
-      # This gets the ids of the specified techs
-      techs = params[:tech].split(' ').map{|x| x.to_i}
-      org_ids = OrganizationTechnology.where(technology_id: techs).pluck('organization_id').uniq
-      org_ids.each do |o|
-        tech << (Organization.where(id: o))
-      end
+    unless params[:term].empty?
+      term = Organization.published.where('name ILIKE ? OR overview ILIKE ?', "%#{params[:term]}%", "%#{params[:term]}%").flatten
     else
-      tech = Organization.all
+      term = nil
     end
-    @results = term & size & tech
+    
+    if params[:size] == "0"
+      size = nil
+    elsif params[:size] == "1"
+      size = Organization.published.where(tech_team_size: 1..25)
+    elsif params[:size] == "2"
+      size = Organization.published.where(tech_team_size: 26..50)
+    elsif params[:size] == "3"
+      size = Organization.published.where('tech_team_size > ?', 50)
+    else
+      flash[:alert] = "Invalid search!"
+    end
+
+    unless params[:tech].empty?
+      techs = []
+      # This gets the ids of the specified techs
+      techs = params[:tech].split('+').map{|x| x.to_i}
+      org_ids = OrganizationTechnology.where(technology_id: techs).pluck('organization_id').uniq
+      techs = []
+      org_ids.each do |o|
+        techs.concat(Organization.published.where(id: o))
+      end
+      techs.flatten!
+    end
+
+    # Elements are nil if no search was executed in the respective category
+    # This then gets the intersection of non-nil search results
+    @results = [term, size, techs].keep_if{|x| x}.reduce(:&)
     respond_to do |format|
       format.html { render json: @results }
       format.json { render json: @results }
