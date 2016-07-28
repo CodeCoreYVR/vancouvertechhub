6b364fe6ba1885468065b8d802d6a8c83d691616
Wilson Lu <huiquanlu@gmail.com>
2016-07-28 11:38:09 -0700

    geocoder vcr test

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -30,8 +30,12 @@ RSpec.describe Organization, type: :model do
     end
 
   it "creates a single pin of V6B 1G8", :vcr do
-    o = Organization.create(address: "142 W Hastings St, Vancouver, BC V6B 1G8, Canada")
-    result = o.valid?
+    o = Organization.create FactoryGirl.attributes_for(:organization).merge({address: "142 W Hastings St, Vancouver, BC V6B 1G8, Canada" })
     expect(o.latitude.round(2)).to eq(49.28)
+  end
+
+  it "doesn't create a pin with invalid address", :vcr do
+    o = Organization.create FactoryGirl.attributes_for(:organization).merge({address: "invalid address" })
+    expect(o.latitude).not_to be
   end
 end
